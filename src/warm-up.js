606e9baa9c377774a0ed111bdec4b0bfe26a6e92
Carlyle Douglas <cdouglascorp@gmail.com>
2020-03-20 11:48:06 -0600

    Complete warm-up

--- src/warm-up.js
+++ src/warm-up.js
@@ -12,31 +12,35 @@
 // var name = "Your name";
 // var ...
 
+var name = "Dougie"
+var age = 32
 
 // Initialize a new variable "someThing", but leave it undefined
 
-
+var someThing
 
 // Initialize a new variable "isHuman", and assign it to a boolean value of your
 // choice
 
-
+var isHuman = true
 
 // Reassign (not initialize) your "age" variable from above. Use a built in math
 // operator to make your age half of what it currently is.
 
-
+age = 32 / 2
 
 // Create a variable named "greeting", and assign it to a greeting of your
 // choice. Your chosen greeting must make use of your "name" variable from above
 
-
+var greeting = `Sup yall, my name is ${name}`
 
 // Create a new variable named "isMinor", and use a comparison operator to
 // assign a boolean value to this variable. If "age" is less than 18, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+
+var isMinor == true
 
 
 
@@ -47,14 +51,22 @@
 // the statement 'Oh! So old!', otherwise, log the statement 'Still a spring
 // chicken!'
 
-
+if (age > 100) {
+  console.log("Oh! So old!")
+} else {
+  console.log("Still a spring chicken!")
+}
 
 // Express the following in code: If the length of the variable name is longer
 // the 5 characters, log the statement 'Hi there [name]!'. Otherwise, log the
 // statement 'Well hello [name]'. In either case, the value stored in the name
 // variable should be included in the logged statement.
 
-
+if (name.length > 5) {
+  console.log(`Hi there ${name}!`)
+} else {
+  console.log(`Well hello ${name}.`)
+}
 
 
 //------------------------- Arrays -------------------------//
@@ -62,14 +74,19 @@
 // Create a new variable "numbers" and assign it to an Array of 10 different
 // numbers
 
-
+var numbers = [101, 22, 73, 40, 65, 16, 72, 38, 19, 6]
 
 // Write the code to access the 5th element of the Array, using bracket
 // notation.
 
+var fifthNumber = numbers[4]
 
 // Create a new variable "planets" and assign it to an Array the 8 planets in
 // our solar system, represented by their names as Strings.
 
+var planets = ["Mercury", "Venus", "Earth", "Mars",
+"Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
 
 // Write the code to access the 7th planet from the sun, using bracket notation.
+
+var seventhRockFromTheSun = planets[6]
